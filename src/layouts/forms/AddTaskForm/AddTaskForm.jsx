accdc984a538bf3e249ec35bb43c1a20f7fc4dd2
Ziad <zgalhelaly@gmail.com>
2023-03-28 03:28:31 +0200

    init

--- src/layouts/forms/AddTaskForm/AddTaskForm.jsx
+++ src/layouts/forms/AddTaskForm/AddTaskForm.jsx
@@ -25,10 +25,10 @@ export default function AddTaskForm() {
 
     return (
         <form
-            className="bg-primary_400 m-3.5 p-3.5 pl-11 rounded-2xl flex flex-wrap gap-5 items-center justify-center hover:text-primary_200 hover:bg-primary_800 [&:hover>fieldset>div:last-child]:text-primary_400 transition"
+            className="bg-primary_400 m-3.5 p-3.5 rounded-2xl flex flex-wrap gap-5 items-center justify-center hover:text-primary_200 hover:bg-primary_800 [&:hover>fieldset>div:last-child]:text-primary_400 transition"
             onSubmit={submitHandler}
         >
-            <fieldset className="flex-1 min-w-fit relative before:absolute before:-left-7 before:top-1.5 before:w-4 before:aspect-square before:bg-primary_500 before:border before:border-primary_900 before:rounded-md">
+            <fieldset className="ml-7 flex-1 min-w-fit relative before:absolute before:-left-7 before:top-1.5 before:w-4 before:aspect-square before:bg-primary_500 before:border before:border-primary_900 before:rounded-md">
                 <legend className="hidden">Create a Task</legend>
                 <div>
                     <label htmlFor="task" className="hidden">
@@ -44,7 +44,7 @@ export default function AddTaskForm() {
                         required
                     />
                 </div>
-                <div className="text-primary_800 flex flex-wrap gap-x-5">
+                <div className="text-primary_800 flex flex-wrap gap-2 mt-2">
                     <div>
                         <label htmlFor="time" className="hidden">
                             Due Time:
@@ -54,8 +54,8 @@ export default function AddTaskForm() {
                             name="time"
                             id="time"
                             ref={timeRef}
-                            className="bg-inherit outline-none"
-                            defaultValue="00:00"
+                            className="bg-inherit outline-none border border-primary_900 rounded-full px-4 py-0.5 cursor-pointer"
+                            // defaultValue="00:00"
                         />
                     </div>
                     <div>
@@ -67,8 +67,8 @@ export default function AddTaskForm() {
                             name="date"
                             id="date"
                             ref={dateRef}
-                            className="bg-inherit outline-none w-32"
-                            defaultValue="2023-12-31"
+                            className="bg-inherit outline-none border border-primary_900 rounded-full px-4 py-0.5 cursor-pointer w-40"
+                            // defaultValue="2023-12-31"
                         />
                     </div>
                 </div>
